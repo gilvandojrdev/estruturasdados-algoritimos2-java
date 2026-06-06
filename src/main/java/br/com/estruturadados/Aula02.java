18833d5229265e190dc8941b78af6f59d75d87b6
gilvando <juniorgilvando16@gmail.com>
2026-06-06 14:30:17 -0300

    Aplicando o metódo de adicionar um elemento no final do vetor com exeções personalizada

---
+++ src/main/java/br/com/estruturadados/Aula02.java
@@ -0,0 +1,18 @@
+package br.com.estruturadados;
+
+import br.com.estruturadados.Entitites.Vetor;
+
+public class Aula02 {
+    static void main(String[] args) {
+        Vetor vetor = new Vetor(2);
+
+        try {
+            vetor.adicionarElemento("elemento01");
+            vetor.adicionarElemento("elemento02");
+            vetor.adicionarElemento("elemento03");
+        }catch (Exception error){
+            error.printStackTrace();
+        }
+
+    }
+}
