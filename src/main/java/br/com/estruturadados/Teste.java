9a2764a501ef55089e30f5b3ea0c1133968201fb
gilvando <juniorgilvando16@gmail.com>
2026-06-06 14:06:47 -0300

    Criação de um arquivo de teste para testar as estruturas de dados.

---
+++ src/main/java/br/com/estruturadados/Teste.java
@@ -0,0 +1,13 @@
+package br.com.estruturadados;
+
+import br.com.estruturadados.Entitites.Vetor;
+
+public class Teste {
+    static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+        int i= 0;
+        i++;
+
+    }
+}
