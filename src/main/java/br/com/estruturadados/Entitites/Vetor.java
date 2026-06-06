8c5527842ef01c466b61dc1303896221089887fa
gilvando <juniorgilvando16@gmail.com>
2026-06-06 14:06:23 -0300

    Criação da classe Vetor e aplicando e fornecendo no construtor a capacidade que esse vetor vai ter

---
+++ src/main/java/br/com/estruturadados/Entitites/Vetor.java
@@ -0,0 +1,12 @@
+package br.com.estruturadados.Entitites;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidadeElementos){
+        this.elementos = new String[capacidadeElementos];
+    }
+
+
+}
