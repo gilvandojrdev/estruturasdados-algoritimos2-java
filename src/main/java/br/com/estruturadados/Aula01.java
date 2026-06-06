82707119b26073c2d9b050ff403392ee5112eb00
gilvando <juniorgilvando16@gmail.com>
2026-06-06 14:12:15 -0300

    Mudança de nome do arquivo para ser em ordem cronólogica.

--- src/main/java/br/com/estruturadados/Aula01.java
+++ src/main/java/br/com/estruturadados/Aula01.java
@@ -2,12 +2,14 @@ package br.com.estruturadados;
 
 import br.com.estruturadados.Entitites.Vetor;
 
-public class Teste {
+public class Aula01 {
     static void main(String[] args) {
 
         Vetor vetor = new Vetor(5);
         int i= 0;
         i++;
+
+
 
     }
 }
