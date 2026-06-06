9da102457b700cecd4c919ec8bcac37c7f26e0f6
gilvando <juniorgilvando16@gmail.com>
2026-06-06 14:12:39 -0300

    Aplicando o metódo de adicionar um elemento no final do vetor

--- src/main/java/br/com/estruturadados/Entitites/Vetor.java
+++ src/main/java/br/com/estruturadados/Entitites/Vetor.java
@@ -8,5 +8,12 @@ public class Vetor {
         this.elementos = new String[capacidadeElementos];
     }
 
-
+    public void adicionarElemento(String elemento){
+        for (int i = 0; i < this.elementos.length; i++) {
+            if(this.elementos[i] == null){
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
+    }
 }
